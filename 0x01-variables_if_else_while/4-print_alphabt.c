ec61ea9a31e3567e8a899e9e98169d83085eac1c
Rhoda Ojetola <momirhoda@gmail.com>
2022-12-10 15:41:19 +0100

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,22 @@
-uy
+#include <stdio.h>
+/**
+ * main - Prints the alphabet except the letters q and e.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			letter++;
+		}
+		putchar(letter)
+    letter++;
+	}
+	putchar('\n');
+	return (0);
+}
