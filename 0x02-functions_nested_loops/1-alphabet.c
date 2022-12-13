67ee56c25a8a235f5c4f99878361b405b8452c60
rh0se <momirhoda@gmail.com>
2022-12-13 15:58:27 -0800

    task13

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,7 @@
 #include "main.h"
-/** print_alphabet- Print alphabet in lowercase using only `_putchar`
- * Return: 0
+/** 
+ * print_alphabet- Print alphabet in lowercase using only `_putchar`
+ * Return: void
  */
 void print_alphabet(void)
 {
