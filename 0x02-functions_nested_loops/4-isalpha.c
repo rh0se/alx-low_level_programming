61862f2e577f5bc9770e6182e19d9f43cc65c54e
rh0se <momirhoda@gmail.com>
2022-12-14 12:54:43 -0800

    task 4 correct

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - checks for alphabetic character
- * Return 1 if letter is lowercase or uppercase
+ * Return 1 if letter c is lowercase or uppercase
  * Return 0 otherwise
  */
 int _isalpha(int c)
