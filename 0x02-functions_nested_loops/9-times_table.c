571be8344bcab6a4f90405502420524da886f40e
rh0se <momirhoda@gmail.com>
2022-12-14 15:10:45 -0800

    task9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * times_table - prints the times table from 0 - 9.
+ * Return: Nothing.
+ */
+void times_table(void)
+{
+	int n, t, times, rem;
+
+	for (n = 0; n < 10; n++)
+	{
+		t = 0;
+		while (t < 10)
+		{
+			times = n * t;
+			rem = times / 10;
+			if (rem < 1 && t != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar(times + '0');
+			_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
