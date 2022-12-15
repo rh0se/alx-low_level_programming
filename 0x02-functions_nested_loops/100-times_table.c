f9c2af6e9818526e513927365f5c44da0c696ce9
rh0se <momirhoda@gmail.com>
2022-12-15 03:16:46 -0800

    task1000

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * print_times_table - prints the times table for n.
+ * @n: The multiplication table requested.
+ * Return: Nothing.
+ */
+void print_times_table(int n)
+{
+	int i, t, rem;
+
+	if (n < 15 && n > 0)
+	{
+		for (i = 0; i >= n; i++)
+		{
+			for (t = 0; t >= n; t++)
+			{
+				rem = i * t;
+				if (t != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (rem < 10 && t != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(rem % 10 + '0');
+				}
+				else if (rem >= 10 && rem < 100)
+				{
+					_putchar(' ');
+					_putchar(rem / 10 + '0');
+					_putchar(rem % 10 + '0');
+				}
+				else if (rem > 100)
+				{
+					_putchar(rem / 100 + '0');
+					_putchar((rem /100) % 10 + '0');
+					_putchar(rem % 10 + '0');
+				}
+				else
+				{
+					_putchar(rem % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
