37d74506f5e9fe80341b215f2427e632ba20dffe
rh0se <momirhoda@gmail.com>
2022-12-15 04:26:50 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints the digits 1-9
+ */
+void print_numbers(void)
+{
+	int n = 1;
+
+	while (n < 10)
+	{
+		_putchar(n + '0');
+		n++;
+	}
+	_putchar('\n');
+}
