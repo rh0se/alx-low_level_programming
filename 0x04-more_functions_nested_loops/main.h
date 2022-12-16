df863ddd7e158ec6591be5f42daa23b34caab6c9
rh0se <momirhoda@gmail.com>
2022-12-16 14:26:40 -0800

    task upper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#!findf MAIN_H
-#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -14,5 +12,3 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 void print_number(int n);
-
-#endif /*MAIN.H*/ 
