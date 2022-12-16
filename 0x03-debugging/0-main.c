0335678041edb95a44549f5822bf4650451feac3
rh0se <momirhoda@gmail.com>
2022-12-16 10:33:07 -0800

    tadk 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - Test function for postive or negative
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 int main(void)
 {
 int i;
 
-i = 98;
+i = 0;
 positive_or_negative(i);
 
 return (0);
