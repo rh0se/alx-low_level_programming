37f152e79037bce49f2f639a14f869579571b0e4
rh0se <momirhoda@gmail.com>
2022-12-15 23:48:27 -0800

    main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN.H
+#define MAIN.H
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,3 +14,5 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 void print_number(int n);
+
+#endif /*MAIN.H*/ 
