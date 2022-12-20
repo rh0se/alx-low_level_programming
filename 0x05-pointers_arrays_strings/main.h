4772b1f866c5c33bc90971e94f9391989abed3d2
rh0se <momirhoda@gmail.com>
2022-12-19 23:40:49 -0800

    main

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H 
+#define MAIN_H 
+#include <stdio.h>
+/**
+ * void prototypes() 
+ *char prototypes() 
+ * int prototypes() 
+ */
+void reset_to_98(int *n); 
+void _puts(char *str); 
+void print_rev(char *s); 
+void rev_string(char *s);
+#endif /*MAIN_H*/
