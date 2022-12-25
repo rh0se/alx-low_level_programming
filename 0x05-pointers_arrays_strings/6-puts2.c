6d3abf3780d59a3b2c43c02aec7d1bc6460a9d0b
rh0se <momirhoda@gmail.com>
2022-12-25 10:39:24 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints string
+ * @str: string to print
+ * Return:nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
