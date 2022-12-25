a2da795e2ecd748704d5f004866c827f08a0763c
rh0se <momirhoda@gmail.com>
2022-12-25 10:51:24 -0800

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to mainpulate
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i, j, half, len = 1;
+
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	if (len % 2 == 0)
+	{
+		half = len / 2;
+		for (j = half; j < len - 1; j++)
+			_putchar(str[j]);
+	}
+	else
+	{
+		half = (len - 1) / 2;
+		for (j = half; j < len - 1; j++)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+}
