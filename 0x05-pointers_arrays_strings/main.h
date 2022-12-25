a13a55e9b8e0fbc3513af66fe888082b092685fa
rh0se <momirhoda@gmail.com>
2022-12-25 02:20:25 -0800

    tadk 2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <math.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 
 void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
 
 #endif /* MAIN_H */
