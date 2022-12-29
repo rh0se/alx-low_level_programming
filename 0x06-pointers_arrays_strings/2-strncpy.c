841c6328b8df4324035801bbceefad810d3e34b8
rh0se <momirhoda@gmail.com>
2022-12-29 11:35:55 -0800

    tadk 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copy string
+ * @dest: pointer char
+ * @src: pointer of char
+ * @n: integer
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		for (; i < n; i++)
+			dest[i] = '\0';
+	}
+	return (dest);
+}
