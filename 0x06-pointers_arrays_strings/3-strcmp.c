6d24d36ee1aa263d3fe61756b851b65d4dccdc33
rh0se <momirhoda@gmail.com>
2022-12-29 11:55:04 -0800

    task 31

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,17 +8,29 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i = 0, diff = 0;
 
-	j = 0;
-
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (1)
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
 		{
-			j = s1[i] - s2[i];
+			diff = s2[i];
 			break;
 		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 	}
-	return (j);
+	return (diff);
 }
